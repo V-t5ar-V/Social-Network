ceef1f93587c61bbcd2a2b3a76fc632337e9c82a
V-t5ar-V <tastemirovaskatbek95@gmail.com>
2026-05-15 17:31:45 +0600

    + Docker

--- SocialNetwork/SocialNetwork/settings.py
+++ SocialNetwork/SocialNetwork/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-am7$w_w36l-)k+xo7_946b$icyoh&l_c=0(0$)exs@vms1#0ig
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['92.38.35.5']
 
 
 # Application definition
