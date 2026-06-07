14c83f5bfda28174de9f7d997f4029f4127fa5e2
V-t5ar-V <tastemirovaskatbek95@gmail.com>
2026-06-07 22:00:17 +0600

    directory fix

--- test_functions/test2.py
+++
@@ -1,9 +0,0 @@
-
-
-class HelloWorld:
-    def __init__(self, text):
-        self.text = text
-        print(self.text)
-
-
-HelloWorld("print")
--- test_functions/test3.py
+++
@@ -1,8 +0,0 @@
-mylist = [1,2,3,4,5,6]
-mylist2 = None
-
-for item in mylist:
-    print(item)
-
-for item in mylist2:
-    print(item)
